74697797acdc9ba71a4922901dfbc47a6ac07a1c
rebin <rebin@rebins-MacBook-Pro.local>
2024-11-20 17:28:07 +0300

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\QuestionsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('questions', QuestionsController::class);
