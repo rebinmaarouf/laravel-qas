efa363576ff5c5a55f624a4e80d5a34979c6d816
rebin <rebin@rebins-MacBook-Pro.local>
2024-11-22 12:20:28 +0300

    Add votes, answer and views counter to question item

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -33,4 +33,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answer > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,15 +9,31 @@
 
                     <div class="card-body">
                         @foreach ($questions as $question)
-                            <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                <p class="lead">
-                                    Asked by
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                {{ Str::limit($question->body, 250) }}
+                            <div class="media">
+                                <div class="float-start counters">
+                                    <div class="vote">
+                                        <strong>{{ $question->votes }}</strong>{{ Str::plural('vote', $question->votes) }}
+                                    </div>
 
+                                    <div class="status {{ $question->status }}">
+                                        <strong>{{ $question->answer }}</strong>{{ Str::plural('answer', $question->answer) }}
+                                    </div>
+
+                                    <div class="views">
+                                        {{ $question->views . ' ' . Str::plural('views', $question->views) }}
+                                    </div>
+                                </div>
+
+                                <div class="float-none">
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                    <p class="lead">
+                                        Asked by
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        <small class="text-muted">{{ $question->created_date }}</small>
+                                    </p>
+                                    {{ Str::limit($question->body, 250) }}
+
+                                </div>
                             </div>
                             <hr>
                         @endforeach
