b342ec1230f1a12a8672365c81bc420ae5290ab5
rebin <rebin@rebins-MacBook-Pro.local>
2024-11-30 16:39:53 +0300

    Update best_anser_id on questions table when answer removed in database layer

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -42,4 +42,9 @@ class Answer extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
 }
